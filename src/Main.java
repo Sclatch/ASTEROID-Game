ed4a6be1d4f2152fd43e5fd5e331edb7bfc5a3e2
Jinhan Liu <jinhan.liu@ontariotechu.net>
2020-03-14 14:59:28 -0400

    Added intro

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,23 @@
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Space Breakout");
+        stage.setWidth(1280);
+        stage.setHeight(800);
+        //stage.setFullScreen(true);
+
+        Intro intro = new Intro();
+        intro.play(stage);
+
+//        HBox hBox = new HBox();
+//        hBox.setPadding(new Insets(50, 50, 50, 50));
+//        hBox.setAlignment(Pos.CENTER);
+//        Scene scene = new Scene(hBox);
+//        stage.setScene(scene);
+//        stage.show();
+    }
 }
